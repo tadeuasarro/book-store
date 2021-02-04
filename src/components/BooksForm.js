f6ccf9d580da4fb59e33c9f65620d9a54036801e
Fernando Bahamondes <ferbaco@gmail.com>
2021-02-04 13:47:36 -0300

    Update BookForm with state and handleChange

--- src/components/BooksForm.js
+++ src/components/BooksForm.js
@@ -1,14 +1,46 @@
+import React, { Component } from 'react';
+
 const CATEGORIES = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
-const BooksForm = () => (
-  <form>
-    <h3 htmlFor="title">Title</h3>
-    <input type="text" id="title" />
-    <select>
-      {CATEGORIES.map(category => <option key={category} value={category}>{category}</option>)}
-    </select>
-    <button type="button">Add Book</button>
-  </form>
-);
+class BooksForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      category: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    console.log(event);
+    this.setState({
+      title: event.target.value,
+      category: event.target.value,
+    });
+  }
+
+  render() {
+    const { title, category} = this.state;
+    return (
+      <form>
+        <h3 htmlFor="title">Title</h3>
+        <input onChange={this.handleChange} type="text" id="title" value={title} />
+        <select>
+          {CATEGORIES.map(category => (
+            <option
+              onChange={this.handleChange}
+              key={category}
+              value={category}
+            >
+              {category}
+            </option>
+          ))}
+        </select>
+        <button type="button">Add Book</button>
+      </form>
+    );
+  }
+}
 
 export default BooksForm;
