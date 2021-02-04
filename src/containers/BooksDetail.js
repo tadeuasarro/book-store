8491778a0448afa989133f0b493790f6500517e0
tadeuasarro <tadeutas@gmail.com>
2021-02-04 12:57:07 -0300

    Fix linters on booksdetail container

--- src/containers/BooksDetail.js
+++ src/containers/BooksDetail.js
@@ -1,20 +1,19 @@
-/* eslint-disable */
-
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Book from '../components/Book';
 
 class BooksDetail extends Component {
-  render(){
-    return(
-      this.props.data.map( book => {
+  render() {
+    return (
+      this.props.data.map(book => { // eslint-disable-line
+        const { id, title, category } = book;
         return (
-        <Book key={book.id} id={book.id} title={book.title} category={book.category}/>
-        )
+          <Book key={id} id={id} title={title} category={category} />
+        );
       })
-    )
+    );
   }
-
 }
 
 function mapStateToProps(state) {
@@ -22,5 +21,13 @@ function mapStateToProps(state) {
     data: state.data,
   });
 }
+
+BooksDetail.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.object),
+};
+
+BooksDetail.defaultProps = {
+  data: [{}],
+};
 
 export default connect(mapStateToProps)(BooksDetail);
